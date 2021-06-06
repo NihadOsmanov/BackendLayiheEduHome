ca48edbb413e33f98162ae7ada31aa171d037d51
Nihad <nihadvos@code.edu.az>
2021-06-07 00:49:20 +0400

    Course Page - Search Created

--- Layihe/Layihe/Controllers/CourseController.cs
+++ Layihe/Layihe/Controllers/CourseController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Layihe.DataAccesLayer;
+using Layihe.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,27 @@ namespace Layihe.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly AppDbContext _dbContext;
+        public CourseController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var courses = _dbContext.Courses.Where(c => c.IsDeleted == false).ToList();
+            return View(courses);
+        }
+
+        public IActionResult Search(string search)
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                return NotFound();
+            }
+
+            var courses = _dbContext.Courses.Where(p => p.Name.Contains(search) && p.IsDeleted == false).Take(8)
+                                                                                                    .OrderByDescending(p => p.Id).ToList();
+            return PartialView("_CourseSearchPartial", courses);
         }
     }
 }
---
+++ Layihe/Layihe/wwwroot/js/layihe.edu.home.js
@@ -0,0 +1,25 @@
+﻿//Course Search
+
+$(document).ready(function () {
+    let search;
+    $(document).on("keyup", "#search-input", function () {
+
+        search = $(this).val().trim();
+        
+        $("#new-courses").empty()
+
+        if (search.length > 0) {
+            $.ajax({
+                url: '/Course/Search?search=' + search,
+                type: "Get",
+                success: function (res) {
+                    $("#old-courses").css("display", "none")
+                    $("#new-courses").append(res)
+                }
+            });
+        }
+        else {
+            $("#old-courses").css("display", "block")
+        }
+    });
+});
